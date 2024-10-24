2f6cedf7e8220782a0a7bcf59285ea8bffc29d2f
ArtdragonXoX <1262922587@qq.com>
2024-10-25 00:18:32 +0800

    数据库连接。

---
+++ bootstrap/db.go
@@ -0,0 +1,7 @@
+package bootstrap
+
+import "yuki-image/db"
+
+func InitDataBase() {
+	db.InitDataBase()
+}
--- db/db.go
+++ db/db.go
@@ -1,1 +1,35 @@
 package db
+
+import (
+	"database/sql"
+	"log"
+	"yuki-image/conf"
+)
+
+var db *sql.DB
+
+func InitDataBase() error {
+	data := conf.Conf.DB
+	var err error
+
+	dsn := data.User + ":" + data.Pwd + "@tcp(" + data.Host + ":" + data.Port + ")/" + data.Name
+	db, err = sql.Open("mysql", dsn)
+	log.Println("Connecting to MySQL:", dsn)
+
+	if err != nil {
+		log.Println("Open database error!")
+		return err
+	}
+	db.SetMaxIdleConns(data.MaxIdle)
+	db.SetMaxOpenConns(data.MaxConn)
+	//defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Println("Error pinging the database!")
+		return err
+	}
+
+	log.Println("Successfully connected to MySQL!")
+	return nil
+}
