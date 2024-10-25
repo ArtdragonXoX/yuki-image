ffd2f71d57c41d3d34f77e8f649265ba61397586
ArtdragonXoX <1262922587@qq.com>
2024-10-26 01:10:18 +0800

    route增加api/v1路由；format查询完成。

--- handlers/route.go
+++ handlers/route.go
@@ -2,16 +2,30 @@ package handlers
 
 import (
 	"yuki-image/conf"
+
+	"github.com/gin-gonic/gin"
 )
+
+var api *gin.RouterGroup
 
 func InitRoute() {
 	server.Static("/i", conf.Conf.Server.Path)
+	api = server.Group("/api/v1")
 	InitAlbum()
+	InitFormat()
 }
 
 func InitAlbum() {
-	albumRoute := server.Group("/album")
+	albumRoute := api.Group("/album")
 	{
 		albumRoute.POST("/", InsertAlbum)
+	}
+}
+
+func InitFormat() {
+	formatRoute := api.Group("/format")
+	{
+		formatRoute.GET("/", SelectAllFormat)
+		formatRoute.GET("/:id", SelectFormat)
 	}
 }
