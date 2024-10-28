dc328572bd185bc149e2e514311f09f69620043c
HEX9CF <hex9cf@aliyun.com>
2024-10-28 15:21:43 +0800

    Fix: Correct json tag in album model

--- internal/model/album.go
+++ internal/model/album.go
@@ -1,19 +1,19 @@
 package model
 
 type Album struct {
-	Id            uint64    `json:"id",omitempty"`
-	Name          string    `json:"name",omitempty`
-	MaxHeight     uint64    `json:"max_height",omitempty"`
-	MaxWidth      uint64    `json:"max_width",omitempty`
+	Id            uint64    `json:"id,omitempty"`
+	Name          string    `json:"name,omitempty"`
+	MaxHeight     uint64    `json:"max_height,omitempty"`
+	MaxWidth      uint64    `json:"max_width,omitempty"`
 	FormatSupport []Format  `json:"format_support,omitempty"`
-	UpdateTime    string    `json:"update_time",omitempty`
-	CreateTime    string    `json:"create_time",omitempty`
-	Image         ImageList `json:"image",omitempty`
+	UpdateTime    string    `json:"update_time,omitempty"`
+	CreateTime    string    `json:"create_time,omitempty"`
+	Image         ImageList `json:"image,omitempty"`
 }
 
 type ImageList struct {
-	Image []Image `json:"image",omitempty`
-	Page  uint64  `json:"page",omitempty`
-	Size  uint64  `json:"size",omitempty`
+	Image []Image `json:"image,omitempty"`
+	Page  uint64  `json:"page,omitempty"`
+	Size  uint64  `json:"size,omitempty"`
 	Total uint64  `json:"total"`
 }
