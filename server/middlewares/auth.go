8b0a531e717d5e66e19918527bc2f2ffefa48add
ArtDragonXoX <1262922587@qq.com>
2024-11-13 16:00:54 +0800

    api鉴权中间件增加管理员token方式。

--- server/middlewares/auth.go
+++ server/middlewares/auth.go
@@ -11,11 +11,16 @@ import (
 func TokenAuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("Authorization")
-
+		v := false
 		// 通常Bearer token前面会有"Bearer "这个字符串，需要去掉它来比较
-		if token != "Bearer "+conf.Conf.Server.Token {
-			c.JSON(http.StatusUnauthorized, model.Response{Code: 0, Msg: "Unauthorized", Data: nil})
-			c.Abort() // 终止请求
+		if token == "Bearer "+conf.Conf.Server.Token {
+			v = true
+		}
+		if err := tokenVerify(c); err == nil {
+			v = true
+		}
+		if !v {
+			c.JSON(http.StatusUnauthorized, model.RespError("token验证失败", nil))
 			return
 		}
 		c.Next() // 继续处理请求
