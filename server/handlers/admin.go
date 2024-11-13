987f64544961af3234033206ca16a54145cf9db1
ArtDragonXoX <1262922587@qq.com>
2024-11-13 11:31:28 +0800

    管理员系统。

---
+++ server/handlers/admin.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"net/http"
+	"yuki-image/internal/admin"
+	"yuki-image/internal/model"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AdminRegister(c *gin.Context) {
+	var u model.User
+	err := c.ShouldBindJSON(&u)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, model.RespError("参数错误", nil))
+		return
+	}
+	err = admin.UserRegister(u)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, model.RespError("注册失败", map[string]string{"err": err.Error()}))
+		return
+	}
+	c.JSON(http.StatusOK, model.RespOk("注册成功", nil))
+}
+
+func AdminLogin(c *gin.Context) {
+	var u model.User
+	err := c.ShouldBindJSON(&u)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, model.RespError("参数错误", nil))
+		return
+	}
+	token, err := admin.UserLogin(u)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, model.RespError("登录失败", map[string]string{"err": err.Error()}))
+		return
+	}
+	c.JSON(http.StatusOK, model.RespOk("登录成功", map[string]string{"token": token}))
+}
